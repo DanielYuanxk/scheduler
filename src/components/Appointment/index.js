cc4cd57faeb4e9a4d7c7639e06d0416c069d9b3d
DanielYuanxk <dyxk1234@hotmail.com>
2023-02-25 23:41:42 -0500

    showing appointments on main page

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,24 @@
 import React from "react";
-import "./styles.scss";
 
+import "./styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  const show = function () {
+    return props.interview ? (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    ) : (
+      <Empty />
+    );
+  };
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {show()}
+    </article>
+  );
 }
